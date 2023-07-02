04c2c18746d6d5289768cf2c09e53a20128c932e
Shakyo choudhury <94360466+Shakyo-choudhury@users.noreply.github.com>
2023-07-02 15:26:30 +0530

    Add files via upload

---
+++ tutorial8.py
@@ -0,0 +1,36 @@
+# Compound Assignment Operators Examples
+
+# Example 1: Addition Assignment (+=)
+x = 10
+x += 5  # Equivalent to: x = x + 5
+print("The new value of x is:", x)  # Output: The new value of x is: 15
+
+# Example 2: Subtraction Assignment (-=)
+x = 20
+x -= 8  # Equivalent to: x = x - 8
+print("The new value of x is:", x)  # Output: The new value of x is: 12
+
+# Example 3: Multiplication Assignment (*=)
+x = 5
+x *= 3  # Equivalent to: x = x * 3
+print("The new value of x is:", x)  # Output: The new value of x is: 15
+
+# Example 4: Division Assignment (/=)
+x = 30
+x /= 6  # Equivalent to: x = x / 6
+print("The new value of x is:", x)  # Output: The new value of x is: 5.0
+
+# Example 5: Modulus Assignment (%=)
+x = 17
+x %= 5  # Equivalent to: x = x % 5
+print("The new value of x is:", x)  # Output: The new value of x is: 2
+
+# Example 6: Exponentiation Assignment (**=)
+x = 3
+x **= 4  # Equivalent to: x = x ** 4
+print("The new value of x is:", x)  # Output: The new value of x is: 81
+
+# Example 7: Floor Division Assignment (//=)
+x = 23
+x //= 4  # Equivalent to: x = x // 4
+print("The new value of x is:", x)  # Output: The new value of x is: 5
